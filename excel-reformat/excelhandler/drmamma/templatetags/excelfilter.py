bd9dc46ec876dbd1efb1c0706a273a3fb58f1f8d
Tedhoon <gt0305@likelion.org>
2020-11-06 12:02:33 +0900

    fix count filter

--- excel-reformat/excelhandler/drmamma/templatetags/excelfilter.py
+++ excel-reformat/excelhandler/drmamma/templatetags/excelfilter.py
@@ -5,12 +5,13 @@ register = template.Library()
 
 @register.filter(name="convert_code_to_name")
 def convert_code_to_name(code): # Only one argument.
-    try:
+    if PRODUCT_CODE.objects.get(code=code):
         CODE_OBJ = PRODUCT_CODE.objects.get(code=code)
         return CODE_OBJ.name
-    except:
+    if Sample.objects.get(sample_dog_code=code):
         CODE_OBJ = Sample.objects.get(sample_dog_code=code)
         return CODE_OBJ.sample_dog_name
-    else:
+    if Sample.objects.get(sample_cat_code=code):
         CODE_OBJ = Sample.objects.get(sample_cat_code=code)
         return CODE_OBJ.sample_cat_name
+    return code
