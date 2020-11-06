955ae6ef8c9f4f4f58ce4450d6c2bad2fc72b87b
Tedhoon <gt0305@likelion.org>
2020-11-06 13:42:57 +0900

    excel filter fix

--- excel-reformat/excelhandler/drmamma/templatetags/excelfilter.py
+++ excel-reformat/excelhandler/drmamma/templatetags/excelfilter.py
@@ -4,14 +4,22 @@ from ..models import PRODUCT_CODE, Sample
 register = template.Library()
 
 @register.filter(name="convert_code_to_name")
-def convert_code_to_name(code): # Only one argument.
-    if PRODUCT_CODE.objects.filter(code=code) is not None:
+def convert_code_to_name(code):
+    # print(PRODUCT_CODE.objects.get(code=code))
+    try: 
         CODE_OBJ = PRODUCT_CODE.objects.get(code=code)
         return CODE_OBJ.name
-    if Sample.objects.filter(sample_dog_code=code) is not None:
+    except:
+        pass
+    try:
         CODE_OBJ = Sample.objects.get(sample_dog_code=code)
         return CODE_OBJ.sample_dog_name
-    if Sample.objects.filter(sample_cat_code=code) is not None:
+    except:
+        pass
+
+    try:
         CODE_OBJ = Sample.objects.get(sample_cat_code=code)
         return CODE_OBJ.sample_cat_name
+    except:
+        pass
     return code
